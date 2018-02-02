bc5593ed968bdabda6abe4979c7500b8927848e1
Hila Shemer <hila-shemer@users.noreply.github.com>
2018-02-02 15:55:18 +0200

    Update (cleanup) imports

--- app/src/main/java/com/hobbyone/HashDroid/MainActivity.java
+++ app/src/main/java/com/hobbyone/HashDroid/MainActivity.java
@@ -26,8 +26,6 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
-import android.graphics.PixelFormat;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -35,7 +33,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.TabHost;
 import android.widget.TextView;
 
